bbe46d47b1891683e309dc97f21f82f3a3e5594c
goliatone <burgosemi@gmail.com>
2017-04-21 12:59:52 -0400

    Export generateFromGUISchema and CompileCommand

--- index.js
+++ index.js
@@ -2,4 +2,8 @@
 
 module.exports = require('./lib/generator');
 
+module.exports.generateFromGUISchema = requrie('./lib/generator').generateFromGUISchema;
+
+module.exports.CompileCommand = require('./commands/compile');
+
 module.exports.GenerateCommand = require('./commands/generate');
